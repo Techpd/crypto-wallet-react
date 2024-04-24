926915130d649b074a5d62fa41b396ac73786950
Prahlad Dixit <dpawan8@gmail.com>
2024-04-24 18:47:07 +0530

    Crypto Wallet Build version 1.0

---
+++ build/precache-manifest.34a5f7c25b40ff26af5535861a5e4104.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de9020b49b1eeeeb8192c18cfee09ba8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63ba8563e29e5de40e0d",
+    "url": "/static/js/2.9a55f249.chunk.js"
+  },
+  {
+    "revision": "afec1d94e48648dc4f6e5802d393952b",
+    "url": "/static/js/2.9a55f249.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1b168599386e482bec2",
+    "url": "/static/js/main.74f0d08e.chunk.js"
+  },
+  {
+    "revision": "c6bc81fc542622f45771",
+    "url": "/static/js/runtime-main.60c452cc.js"
+  }
+]);
